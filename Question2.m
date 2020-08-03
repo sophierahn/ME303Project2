fa3be729493b953d66f394ed31cd3805f9603e7c
sophierahn <44484902+sophierahn@users.noreply.github.com>
2020-08-03 11:57:02 -0400

    Update Question2.m
    Final Version

--- Question2.m
+++ Question2.m
@@ -1,73 +1,85 @@
-%Clears everything from before
-clc
-clear all
+%ME 303 Project 2
+%Question 2 part 1
+%Cooling flatbread with DBCs
+%Written by Sophia Rahn
 
-% Limits
+
+%Clear everything
+clc
+clear all;
+
+%Dimensional Limits
 y_limit = 1;
 x_limit = 1;
-t_limit = 1; % but also infinity
+t_limit = 0.1; % but also as long as we want
 
-dt = 0.0001;
+%Time step
+dt = 0.00001;
 
-y_slots = 10;
-x_slots = 10;
-% t_slots = round(t_limit/dt,0);
-t_slots = 1000;
+%Number of discrete samples
+y_slots = 100;
+x_slots = 100;
+t_slots = round(t_limit/dt,0);
 
+%Creating linearly spaced dimensional vectors
 x = linspace(0,x_limit, x_slots);
 y = linspace(0,y_limit,y_slots);
+
+%Done to improve readibility of the final array print
 y = transpose(y);
 
+
+%Dimensional steps
 dx = x(2)-x(1);
 dy = y(2)-y(1);
 
-%T(x,y,t)
+%Initializing T(x,y,t)for the number of samples
 T = ones(x_slots,y_slots,t_slots);
 
-g= 0;
+%Doing some math before it's needed
 X = pi.*x;
 Y = (4*pi).*y;
 
 %ICs
 T(:,:,1) = T(:,:,1).*sin(X).*sin(Y);
 
+%Transposing dimensional vectors for proper array multiplication
 X = transpose(X);
 X2 = (2*pi).*x;
 X2 = transpose(X2);
 
 %BCs
-    T(1,:,:) = 0;
-    T(end,:,:) = 0;
-    T(:,1,:) = T(:,1,:).*sin(X);
-    T(:,end,:) = T(:,end,:).*(cos(X2)-1);
+T(1,:,:) = 0;
+T(end,:,:) = 0;
+T(:,1,:) = T(:,1,:).*sin(X);
+T(:,end,:) = T(:,end,:).*(cos(X2)-1);
     
-%constants
+%Constants
 F = dt/(dx^2);
 G = dt/(dy^2);
 
 
 
-%Iterative calculation of T(i,j)
-for k=1:t_slots
-    for i = 2:(x_slots-1)
-        for j = 2:(y_slots-1)
-            % Interior points using central difference
+%Iterative calculation of T(i,j,k)
+for k=1:t_slots %iterating through time
+    for i = 2:(x_slots-1) %iterating through x
+        for j = 2:(y_slots-1) %iterating through y
             T(i,j,k+1) = (F*(T(i+1,j,k)-(2*T(i,j,k))+T(i-1,j,k)))+(G*(T(i,j+1,k)-(2*T(i,j,k))+T(i,j-1,k)))+T(i,j,k);
         end
     end
     
-%BCs
-    T(1,:,:) = 0;
-    T(end,:,:) = 0;
-    T(:,1,k+1) =sin(X);
-    T(:,end,k+1) = (cos(X2)-1);
+%BCs to ensure they were not overwritten
+T(1,:,:) = 0;
+T(end,:,:) = 0;
+T(:,1,k+1) = sin(X);
+T(:,end,k+1) = (cos(X2)-1);
 
+%Plotting the temperature array
 surf(T(:,:,k+1));
+title('Temperature vs Space iterating through Time');
+xlabel('X');
+ylabel('Y');
+zlabel('Temperature');
 pause(0.1);
 
 end
-% j= ones(x_slots,y_slots);
-% j(:,:) = T(:,:,1);
-% heatmap(j,'colormap', hot)
-
-% surf(T(:,:,end));
